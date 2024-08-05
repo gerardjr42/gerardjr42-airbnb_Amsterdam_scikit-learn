1d62c3632c0147507de0bc69272321606298e6dc
gerardjr42 <103994562+gerardjr42@users.noreply.github.com>
2024-08-05 00:29:10 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ model = pickle.load(open("model.pkl", "rb"))
 
 st.title("The Airbnb dataset of Amsterdam")
 st.markdown(
-    "This project leverages the power of Python's data science ecosystem, including NumPy, Pandas, Plotly, and Scikit-learn, to analyze the Airbnb market in Amsterdam.  By uncovering insights into pricing trends, popular neighborhoods, and influential amenities, the project delivers a user-friendly platform for exploring the dataset and predicting Airbnb listing prices."
+    "This project leverages the power of Python's data science ecosystem, including NumPy, Pandas, Plotly, and Scikit-learn, to analyze the Airbnb market in Amsterdam.  By uncovering insights into pricing trends, based on amenities, accomodates, and if it's instantly bookable. The project delivers a user-friendly platform for exploring the dataset and predicting Airbnb listing prices."
 )
 
 amenities = st.slider('How many amenities does the listing have?', 0, 50, 20)
@@ -19,4 +19,4 @@ instant_bookable = 1 if instant_bookable == "True" else 0
 user_input = [[amenities, accommodates, instant_bookable]]
 
 if st.button('Predict?'):
-    st.write("The model predicts that the average tip for this listing is:", model.predict(user_input).round(2))
+    st.write("The model predicts that the average price for this listing is:", model.predict(user_input).round(2))
